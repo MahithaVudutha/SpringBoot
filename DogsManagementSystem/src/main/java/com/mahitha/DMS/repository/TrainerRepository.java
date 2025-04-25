07e2c8c696fe8775a3146f9b48cd22fe495319f6
Mahitha Vudutha <143000682+MahithaVudutha@users.noreply.github.com>
2025-04-24 19:09:16 -0500

    Add files via upload

---
+++ DogsManagementSystem/src/main/java/com/mahitha/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.mahitha.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mahitha.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
